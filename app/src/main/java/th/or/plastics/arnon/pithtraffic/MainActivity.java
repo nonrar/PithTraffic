f8a5ca3d8c26fcb58b306a9deba8f69d30c1488e
arnon <infononrar@gmail.com>
2015-11-19 14:20:00 +0700

    Bind Widget เสร็วแล้ว

--- app/src/main/java/th/or/plastics/arnon/pithtraffic/MainActivity.java
+++ app/src/main/java/th/or/plastics/arnon/pithtraffic/MainActivity.java
@@ -2,12 +2,44 @@ package th.or.plastics.arnon.pithtraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Explicit ทำการประกาศตัวแปล
+    // public ใช้นอก class ได่้
+    // private ใช้ภายใน class เท่านั้น
+
+    private Button aboutMeButton;
+    private ListView trafficListView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //Statement จะจบด้วย ;
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+
+        //การตั้งชื่อ method อักษรตัวแรกควรเป็นตัวเล็ก
+        bindWidget();
+
+
+
+
+
+    } // Main Method
+
+    private void bindWidget() {
+
+        aboutMeButton = (Button) findViewById(R.id.button);
+        trafficListView = (ListView) findViewById(R.id.listView);
+
+
     }
-}
+
+} // Main Class
+
+
